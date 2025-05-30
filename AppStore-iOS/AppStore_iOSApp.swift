863ca9ede9306488966e694cece434562a20e413
MinaShehata <minashehatacs@gmail.com>
2025-05-30 09:36:53 +0200

    Initial Commit

---
+++ AppStore-iOS/AppStore_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStore_iOSApp.swift
+//  AppStore-iOS
+//
+//  Created by Mina Shehata on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AppStore_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
